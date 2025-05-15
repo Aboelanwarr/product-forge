6e521492e3e0be09f5bb837d8ff142c1cf278432
aboelanwarr <o.b1277@yahoo.com>
2025-05-15 19:21:51 +0300

    feat: add product validation and error handling in form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import { useState, type ChangeEvent } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import ProductCard from "./components/ProductCard";
 import MyModal from "./components/ui/Modal";
 import { formInputsList, productList } from "./data";
 import Button from "./components/ui/Button";
-import Input from "./components/ui/input";
 import type { IProduct } from "./interfaces";
+import { productValidation } from "./validation";
+import Input from "./components/ui/input";
+import ErrorMessage from "./components/ui/ErrorMessage";
 
 function App() {
-  /* -------------- STATE -------------- */
-  const [product, setProduct] = useState<IProduct>({
+  const defaultProductObj = {
     title: "",
     description: "",
     imageURL: "",
     price: "",
     colors: [],
     category: {
-      name: '',
-      imageURL:''
-    }
+      name: "",
+      imageURL: "",
+    },
+  };
+  /* -------------- STATE -------------- */
+  const [product, setProduct] = useState<IProduct>(defaultProductObj);
+  const [errors, setErrors] = useState({
+    title: "",
+    description: "",
+    imageURL: "",
+    price: "",
   });
   const [isOpen, setIsOpen] = useState(false);
+  console.log("errors",errors);
 
   /* -------------- HANDLER -------------- */
   const closeModal = () => setIsOpen(false);
@@ -30,21 +40,55 @@ function App() {
       ...product,
       [name]: value,
     });
+    setErrors({
+      ...errors,
+      [name]: ""
+    });
   };
-  console.log(product);
+  const onCancel = () => {
+    setProduct(defaultProductObj);
+    closeModal();
+  };
+  const submitHandler = (event: FormEvent<HTMLFormElement>): void => {
+    const { title, description, price, imageURL } = product;
+    event.preventDefault();
+    const errors = productValidation({
+      title,
+      description,
+      imageURL,
+      price,
+    });
+
+    const hasErrorMsg =
+      Object.values(errors).some((value) => value === "") &&
+      Object.values(errors).every((value) => value === "");
+    console.log(hasErrorMsg);
+    if (!hasErrorMsg) {
+      setErrors(errors);
+      return;
+    }
+    console.log("SEND THIS PRODCUT TO OUR SERVER");
+  };
   /* -------------- RENDER -------------- */
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
   const renderFormInputList = formInputsList.map((input) => (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2" key={input.id}>
       <label
         htmlFor={input.id}
         className="mb-[2px] text-sm font-medium text-gray-700"
       >
         {input.label}
       </label>
-      <Input type="text" id={input.id} name={input.name} value={product[input.name]} onChange={onChangeHandler} />
+      <Input
+        type="text"
+        id={input.id}
+        name={input.name}
+        value={product[input.name]}
+        onChange={onChangeHandler}
+      />
+      <ErrorMessage msg={errors[input.name]} />
     </div>
   ));
 
@@ -61,13 +105,18 @@ function App() {
         closeModal={closeModal}
         title="ADD A NEW PRODUCT"
       >
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={submitHandler}>
           {renderFormInputList}
           <div className="flex items-center space-x-3">
             <Button className="bg-indigo-700 hover:bg-indigo-800">
               Submit
             </Button>
-            <Button className="bg-gray-500 hover:bg-gray-800">Cancel</Button>
+            <Button
+              className="bg-gray-500 hover:bg-gray-800"
+              onClick={onCancel}
+            >
+              Cancel
+            </Button>
           </div>
         </form>
       </MyModal>
